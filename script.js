a4412a8941ea91e0c279e487bfeb2d4e7a599b11
mariummun <munnimarium5@gmail.com>
2026-06-19 00:02:41 +0600

    more basic practice

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
-let heading = document.querySelector("h2");
-console.dir(heading.append(" with others"));
+// let heading = document.querySelector("h2");
+// console.dir(heading.append(" with others"));
+
+let div = document.querySelectorAll(".box");
+
+div[0].innerText = "First hello from the other side";
+div[1].innerText = "Second hello from the other side";
+div[2].innerText = "Third hello from the other side";
