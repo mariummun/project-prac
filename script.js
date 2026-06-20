c33cbbbea8feaa72c8f3affc3b3c1281311af79e
mariummun <munnimarium5@gmail.com>
2026-06-20 23:49:50 +0600

    get method

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 // let heading = document.querySelector("h2");
 // console.dir(heading.append(" with others"));
 
-let divs = document.querySelectorAll(".box");
+// let divs = document.querySelectorAll(".box");
 
 // divs[0].innerText = "hello from the other side 1";
 // divs[1].innerText = "hello from the other side 2";
 // divs[2].innerText = "hello from the other side 3";
+//OR
+// let idx = 1;
+// for(div of divs){
+//     div.innerText = `hello from the other side ${idx}`;
+//     idx++;
+// }
 
-let idx = 1;
-for(div of divs){
-    div.innerText = `hello from the other side ${idx}`;
-    idx++;
-}
+let para = document.querySelector("p");
+console.log(para.getAttribute("class")); //returns the attribute name of the class
